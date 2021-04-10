425ced3386bab25a62c1fbf19e07dd28285379f2
Christian Stevens <stev1905@gmail.com>
2021-04-10 10:42:17 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './header/header.component';
+import { Wheel } from 'react-custom-roulette';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'chris'
+    }
+  }
+  render() {
+    const data = [
+      { option: 'Chris'},
+      { option: 'Shawn'},
+      { option: 'Neilson'},
+      { option: 'Jen'},
+      { option: 'Laura'},
+      { option: 'Alek'},
+      { option: 'Vicky'},
+      { option: 'Jerrycka'}
+    ]
+    return (
+      <div>
+      <Header />
+        <Wheel
+          mustStartSpinning={true}
+          prizeNumber={6}
+          innerRadius={10}
+          data={data}
+          textColors={['black']}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/header/header.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+
+const Header = () => (
+  <>
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Sneaker Raffle</Navbar.Brand>
+    </Navbar>
+  </>
+);
+
+export default Header;
